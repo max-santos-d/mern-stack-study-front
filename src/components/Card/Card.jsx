523b2e0f60320bedfd440944fc282ddc3f66fab3
Max Santos <max.santosdev@gmail.com>
2024-07-31 14:08:32 -0300

    Desenvolvimento do componente Card e aplicação de sua distribuição na página Home

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,29 @@
-export function Card({news}) {
+import { CardContainer, CardContent, CardInteractions } from "./Styled";
+
+export function Card({ news }) {
     return (
         <>
-            <section>                
-                <h2>{news.title}</h2>
-                <p>{news.text}</p>
-                <img src={news.banner} alt="Imagem" />
-                <i className="bi bi-heart"></i>
-                <span>{news.likes}</span>
-                <i className="bi bi-chat"></i>
-                <span>{news.comments}</span>
-            </section>
+            <CardContainer>
+                <CardContent>
+                    <div>
+                        <h2>{news.title}</h2>
+                        <p>{news.text}</p>
+                    </div>
+                    <img src={news.banner} alt="Imagem" />
+                </CardContent>
+
+                <CardInteractions>
+                    <div>
+                        <i className="bi bi-heart"></i>
+                        <span>{news.likes}</span>
+                    </div>
+
+                    <div>
+                        <i className="bi bi-chat"></i>
+                        <span>{news.comments}</span>
+                    </div>
+                </CardInteractions>
+            </CardContainer>
         </>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,16 @@
 import { Navbar } from "../../components/Navbar/Navbar";
 import { Card } from '../../components/Card/Card';
 import news from '../../Data'
+import { HomeContainer } from "./Styled";
 
 export default function Home() {
 
     return (
         <>
             <Navbar />
-            {news.map((item, index) => (<Card key={index} news={item} />))}
+            <HomeContainer>
+                {news.map((item, index) => (<Card key={index} news={item} />))}
+            </HomeContainer>
         </>
     )
 };
